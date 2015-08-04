17fbaf08dae85a3b1ae1c5d7d7df5163be31b53c
Andras Sevcsik <sevcsik@gmail.com>
2015-08-05 01:14:38 +0200

    project support, eplayer project

--- resume/components/project.js
+++ resume/components/project.js
@@ -12,8 +12,8 @@ Polymer({
 		let elements = Polymer.dom(this).querySelectorAll('[buzzwords] > *');
 		this._buzzwords = [...elements].map((element) => element.textContent);
 
-		if (Polymer.dom(this).querySelector('img.cover')) {
-			Polymer.dom(this.root).addClass('has-image');
+		if (Polymer.dom(this).querySelector('img')) {
+			Polymer.dom(this.$.header).classList.add('has-image');
 		}
 	}
 });
