8c7516fab00c47c8fbd1026a6e62a5f677e09fec
Andras Sevcsik <sevcsik@gmail.com>
2015-08-08 01:23:36 +0200

    Added contact style for screamingbox

--- resume/components/contact.js
+++ resume/components/contact.js
@@ -10,12 +10,23 @@ Polymer.Base.importHref('components/contact.html', function() {
 Polymer({
 	is: 'sd-contact',
 	ready() {
+		if (branding) {
+			this.classList.add(`branding-${branding}`);
+		}
+
 		if (branding === 'screamingbox') {
 			let email = Polymer.dom(this).querySelector('.contact-info .email');
 			
 			if (email) {
 				email.textContent = 'andras.sevcsik@screamingbox.com';
 				email.href = `mailto:${email.textContent}`;
+			}
+
+			let site = Polymer.dom(this).querySelector('.contact-info .site');
+			
+			if (site) {
+				site.textContent = 'screamingbox.com';
+				site.href = `http://${site.textContent}`;
 			}
 		}
 	}
