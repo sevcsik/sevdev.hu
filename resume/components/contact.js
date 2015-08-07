d74959caf2fbc0caa751ed784a4917ee3856ee29
Andras Sevcsik <Andras.Sevcsik@performgroup.com>
2015-08-07 19:38:30 +0200

    change email on branding

--- resume/components/contact.js
+++ resume/components/contact.js
@@ -1,11 +1,24 @@
+"format es6"
 /**
  * Contact information (cover)
  */
+
+import branding from 'branding.js';
 
 Polymer.Base.importHref('components/contact.html', function() {
 
 Polymer({
 	is: 'sd-contact',
+	ready() {
+		if (branding === 'screamingbox') {
+			let email = Polymer.dom(this).querySelector('.contact-info .email');
+			
+			if (email) {
+				email.textContent = 'andras.sevcsik@screamingbox.com';
+				email.href = `mailto:${email.textContent}`;
+			}
+		}
+	}
 });
 
 });
