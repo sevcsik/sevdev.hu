aa762d14c34c6a03a8558fc2deaf5afb8b9f4ee2
Andras Sevcsik <sevcsik@gmail.com>
2016-01-08 02:53:25 +0100

    added angular directive for splash

--- scripts/splash.ts
+++ scripts/splash.ts
@@ -1,2 +1,6 @@
-interface Hello {};
+require("angular2/bundles/angular2-polyfills");
 
+import { bootstrap } from 'angular2/platform/browser';
+import { SplashComponent } from './splash/splash.component.ts';
+
+bootstrap(SplashComponent);
---
+++ scripts/splash/splash.component.ts
@@ -0,0 +1,7 @@
+import { Component } from 'angular2/core';
+
+@Component({ selector: 'sd-splash'
+           , template: 'hello ng2'
+           })
+ 
+export class SplashComponent { }
