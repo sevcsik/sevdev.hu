f5f8072be2078b2b66b11a2384fa20694a2a4355
Sevcsik-Zajacz, Andras <andras.sevcsik@misys.com>
2018-10-01 18:46:06 +0200

    Fixed locale issue in nix build

--- blog/default.nix
+++ blog/default.nix
@@ -1,6 +1,7 @@
 { stdenv
+, glibcLocales
 , generator ? import ../generator/release.nix
-, glibcLocales }:
+}:
 let ignores = [ "_cache" "_site" "result" ];
 in stdenv.mkDerivation rec {
 	name = "sevdev-blog";
@@ -9,10 +10,9 @@ in stdenv.mkDerivation rec {
 		(path: _: builtins.foldl' (acc: el: acc && builtins.baseNameOf path != el) true ignores)
 		./.;
 	phases = "unpackPhase buildPhase";
-	buildInputs = [ generator, glibcLocales ];
+	buildInputs = [ generator glibcLocales ];
 	buildPhase = ''
 		export LANG=en_US.UTF-8
-		export LC_ALL=en_US.UTF-8
 		chmod -R u+w *
 		site rebuild
 		mkdir $out
