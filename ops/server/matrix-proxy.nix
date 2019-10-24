6813184c77c1321232712ff0a2e554fe26080954
Sevcsik-Zajácz András <sevcsik-zajacz.andras@gbsolutions.io>
2019-10-24 14:46:37 +0200

    fixed url canonization issue with proxied requests

--- ops/server/matrix-proxy.nix
+++ ops/server/matrix-proxy.nix
@@ -1,7 +1,7 @@
 { nodes, pkgs, resources, ... }:
 let matrixVHost = { enableACME = true
                   ; forceSSL = true
-                  ; locations."/_matrix" = { proxyPass = "http://localhost:8448/_matrix"
+                  ; locations."/_matrix" = { proxyPass = "http://127.0.0.1:8448$request_uri"
                                            ; extraConfig = ''
                                                proxy_set_header X-Forwarded-For $remote_addr;
                                              ''
@@ -11,7 +11,8 @@ let matrixVHost = { enableACME = true
     ${pkgs.openssh}/bin/ssh-keyscan ${matrixHostname} >> /root/.ssh/known_hosts
   ''
 ; matrixHostname = nodes.matrix.config.networking.hostName
-; sshTunnelCmd = "${pkgs.openssh}/bin/ssh -i /etc/matrix-tunnel-key -L 8448:localhost:8448 -N matrix-tunnel@${matrixHostname}"
+; sshTunnelCmd = "${pkgs.openssh}/bin/ssh -i /etc/matrix-tunnel-key -L 8448:localhost:8448 " +
+                 "-N matrix-tunnel@${matrixHostname}"
 ; in
 { environment.etc."matrix-tunnel-key" = { mode = "600" # FIXME: this stores the key insecurely in /nix/store.
                                         ; text = resources.sshKeyPairs.matrixTunnel.privateKey
